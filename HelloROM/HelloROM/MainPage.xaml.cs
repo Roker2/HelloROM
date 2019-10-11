537042165855795bb44a041ecdeefcad45b47e64
Roker2 <dimaminko2000@gmail.com>
2019-10-11 15:08:32 +0300

    MainPage: Add description for Lineage OS

--- HelloROM/HelloROM/MainPage.xaml.cs
+++ HelloROM/HelloROM/MainPage.xaml.cs
@@ -23,6 +23,7 @@ namespace HelloROM
             ROMArray[0].AddScreenshots("https://github.com/Roker2/HelloROMScreenshots/raw/master/CarbonROM/", 7, ".png");
             ROMArray[1].AddScreenshots("https://github.com/Roker2/HelloROMScreenshots/raw/master/Lineage%20OS/", 6, ".jpg");
             ROMArray[2].AddScreenshots("https://www.resurrectionremix.com/img/screenshots/screenshot_0", 6, ".png");
+            ROMArray[1].Description = "It is the successor to the custom ROM CyanogenMod, from which it was forked in December 2016 when Cyanogen Inc. announced it was discontinuing development and shut down the infrastructure behind the project.\n© Wikipedia";
             ROMs rOMs = new ROMs(ROMArray);
             ROMList.ItemsSource = rOMs;
         }
