853bc9abb9e77516ba8e1188d0a55cb9e62471c7
Roker2 <dimaminko2000@gmail.com>
2019-10-13 18:07:10 +0300

    PageAddROM: Add screenshots to adding ROM

--- HelloROM/HelloROM/PageAddROM.xaml.cs
+++ HelloROM/HelloROM/PageAddROM.xaml.cs
@@ -12,6 +12,10 @@ namespace HelloROM
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PageAddROM : ContentPage
 	{
+        int ScreensNumber = 0;
+
+        List<Entry> entryMass = new List<Entry> { };
+
 		public PageAddROM()
 		{
 			InitializeComponent ();
@@ -21,7 +25,27 @@ namespace HelloROM
         {
             ROM PreviewROM = new ROM(ROMName.Text, ROMBase.Text, PicURL.Text, ROMSite.Text, ROMGerrit.Text, ROMGithub.Text);
             PreviewROM.AddDescription(ROMDescription.Text);
+            PreviewROM.Screenshots.Clear();
+            for(int i = 0; i < ScreensNumber; i++)
+            {
+                PreviewROM.AddScreenshot(entryMass[i].Text);
+            }
             await Navigation.PushAsync(new ROMPage(PreviewROM));
+        }
+
+        private void ScreenshotsNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Screenshots.Children.Clear();
+            entryMass.Clear();
+            if(ScreenshotsNumber.Text != "")
+            {
+                ScreensNumber = Convert.ToInt32(ScreenshotsNumber.Text);
+                for (int i = 0; i < ScreensNumber; i++)
+                {
+                    entryMass.Add(new Entry());
+                    Screenshots.Children.Add(entryMass[i]);
+                }
+            }
         }
     }
 }
