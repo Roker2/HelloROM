fef7757e16a27d47f9a8841b06794312a83728bb
Roker2 <dimaminko2000@gmail.com>
2019-10-13 15:38:28 +0300

    MainPage: Remove ROMs massive
    I use json from Github instead massive

--- HelloROM/HelloROM/MainPage.xaml.cs
+++ HelloROM/HelloROM/MainPage.xaml.cs
@@ -17,19 +17,7 @@ namespace HelloROM
         public MainPage()
         {
             InitializeComponent();
-            ROM[] ROMArray = new ROM[]
-            {
-                new ROM("CarbonROM", "AOSP", "https://github.com/Roker2/HelloROM/raw/master/Images/CarbonROM.png", "https://carbonrom.org", "https://review.carbonrom.org", "https://github.com/CarbonROM"),
-                new ROM("Lineage OS", "AOSP", "https://avatars3.githubusercontent.com/u/24304779", "https://lineageos.org", "https://review.lineageos.org", "https://github.com/LineageOS"),
-                new ROM("Resurrection Remix", "Lineage OS", "https://avatars3.githubusercontent.com/u/4931972", "https://www.resurrectionremix.com", null, "https://github.com/ResurrectionRemix")
-            };
-            ROMArray[0].AddScreenshots("https://github.com/Roker2/HelloROMScreenshots/raw/master/CarbonROM/", 7, ".png");
-            ROMArray[1].AddScreenshots("https://github.com/Roker2/HelloROMScreenshots/raw/master/Lineage%20OS/", 6, ".png");
-            ROMArray[2].AddScreenshots("https://www.resurrectionremix.com/img/screenshots/screenshot_0", 6, ".png");
-            ROMArray[1].Description = "It is the successor to the custom ROM CyanogenMod, from which it was forked in December 2016 when Cyanogen Inc. announced it was discontinuing development and shut down the infrastructure behind the project.\n© Wikipedia";
-            ROMs rOMs = new ROMs(ROMArray);
             GetStat();
-            //ROMList.ItemsSource = rOMs;
         }
 
         private async void GetStat()
