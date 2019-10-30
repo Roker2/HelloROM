de8ea2845026751838c40888bf16ca37e32d6da5
Roker2 <dimaminko2000@gmail.com>
2019-10-30 17:22:35 +0300

    SortTypes: Add summaries

--- HelloROM/HelloROM/SortTypes.cs
+++ HelloROM/HelloROM/SortTypes.cs
@@ -6,7 +6,13 @@ namespace HelloROM
 {
     enum SortTypes
     {
+        /// <summary>
+        /// Sort type "Name"
+        /// </summary>
         Name,
+        /// <summary>
+        /// Sort type "Base"
+        /// </summary>
         Base
     }
 }
