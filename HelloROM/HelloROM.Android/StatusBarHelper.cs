b02848b2361a96e65c8153840892c4e07a40a154
Roker2 <dimaminko2000@gmail.com>
2019-11-22 17:47:01 +0300

    Add code for hide and show status bar
    From https://ru.stackoverflow.com/questions/751463/%D0%9A%D0%B0%D0%BA-%D1%83%D0%B1%D1%80%D0%B0%D1%82%D1%8C-action-bar-%D0%B2-xamarin-forms

---
+++ HelloROM/HelloROM.Android/StatusBarHelper.cs
@@ -0,0 +1,49 @@
+﻿using Xamarin.Forms;
+using Android.Support.V7.App;
+using Android.Views;
+
+[assembly: Dependency(typeof(HelloROM.Droid.StatusBarHelper))]
+
+namespace HelloROM.Droid
+{
+    public class StatusBarHelper : HelloROM.IStatusBar
+    {
+        private WindowManagerFlags _originalFlags;
+        private bool IsHide { get; set; }
+
+        #region IStatusBar implementation
+
+        /// <summary>
+        /// Hide
+        /// </summary>
+        public void HideStatusBar()
+        {
+            if (IsHide) return;
+
+            IsHide = true;
+
+            var activity = (AppCompatActivity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            _originalFlags = attrs.Flags;
+            attrs.Flags |= WindowManagerFlags.Fullscreen;
+            activity.Window.Attributes = attrs;
+        }
+
+        /// <summary>
+        /// Show
+        /// </summary>
+        public void ShowStatusBar()
+        {
+            if (!IsHide) return;
+
+            IsHide = false;
+
+            var activity = (AppCompatActivity)Forms.Context;
+            var attrs = activity.Window.Attributes;
+            attrs.Flags = _originalFlags;
+            activity.Window.Attributes = attrs;
+        }
+
+        #endregion
+    }
+}
---
+++ HelloROM/HelloROM/IStatusBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloROM
+{
+    public interface IStatusBar
+    {
+        /// <summary>
+        /// Hide Status Bar
+        /// </summary>
+        void HideStatusBar();
+
+        /// <summary>
+        /// Show Status Bar
+        /// </summary>
+        void ShowStatusBar();
+    }
+}
