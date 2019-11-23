3e9d2b0e2aa71a25ea20fbb47f9dd60577e2b0a9
Roker2 <dimaminko2000@gmail.com>
2019-11-23 20:07:48 +0300

    AboutProgram: Init

---
+++ HelloROM/HelloROM/AboutProgram.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloROM
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutProgram : ContentPage
+    {
+		public AboutProgram ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
