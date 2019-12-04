5ce60d569970e7fb1fed6ae892190e505e36a64c
Roker2 <dimaminko2000@gmail.com>
2019-12-04 18:59:57 +0300

    Utils: Init

---
+++ HelloROM/HelloROM/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloROM
+{
+    public static class Utils
+    {
+        public static string GetTranslation(string name)
+        {
+            object netLanguage;
+            if (!App.Current.Properties.TryGetValue("netLanguage", out netLanguage))
+            {
+                netLanguage = "en";
+            }
+            ResourceManager rm = new ResourceManager("HelloROM.Translations.Translation", typeof(Translations.Translation).Assembly);
+            return rm.GetString(name, new System.Globalization.CultureInfo((string)netLanguage));
+        }
+    }
+}
